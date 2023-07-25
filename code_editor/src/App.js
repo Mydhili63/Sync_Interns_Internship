b2b2d6fef12f3a5eb590042c266468329981c603
Mydhili63 <y.mydhili63@gmail.com>
2023-07-24 17:53:24 -0900

    internship

---
+++ code_editor/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from './components/Home';
+import Dataprovider from './context/Dataprovider';
+
+function App() {
+  return (
+    <Dataprovider>
+      <Home/>
+    </Dataprovider>
+  );
+}
+
+export default App;
